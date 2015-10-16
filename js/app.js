c5cd4ef457d57d3f3a6b9d568f93e16539b28f4c
galdinorosas <dino.rosas@gmail.com>
2015-10-16 02:14:20 -0700

    made changes on h button click

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 
-	})
-	.mouseleave(function(){
+	});
+	$('.ryu').mouseleave(function(){
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
 
@@ -15,24 +15,48 @@ $(document).ready(function() {
 	$('.ryu').mousedown(function() {
     	$('.ryu-ready').hide();
     	$('.ryu-throwing').show();
-    	$('.hadouken').finish().show().animate({"left":"1020px"},600,function(){
+    	$('.hadouken').show().finish().animate({"left":"1020px"},600,function(){
     		playHadouken();
     		$(this).hide();
     		$(this).css("left","520px");
-    		
-    	});
+    		});
+    });
     // play hadouken sound
     // show hadouken and animate it to the right of the screen
-  	})
  	$('.ryu').mouseup(function() {
     	 $('.ryu-throwing').hide();
  		 $('.ryu-ready').show();
     // ryu goes back to his ready position
  	});
-	});
 
 	function playHadouken () {
 		$('#hadouken-sound')[0].volume = 0.5;
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
-}
+		};
+
+	$(window).keydown(function(e){
+
+		if(e.keyCode==72){
+			$('.ryu-cool').show();
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+
+		}
+	});
+
+	$(window).keyup(function(e){
+
+		if(e.keyCode==72){
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+			$('.ryu-ready').hide();
+
+
+		}
+	});
+
+
+
+});
+
